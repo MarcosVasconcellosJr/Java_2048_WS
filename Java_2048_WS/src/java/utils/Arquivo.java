f60559ca176e1e867235e32b655e41c25ec14c64
marcos-vasconcellos-mblabs <marcos.vasconcellos@mblabs.com.br>
2019-10-26 16:48:13 -0300

    Solved error read archive with last command buffered in the archive

--- Java_2048_WS/src/java/utils/Arquivo.java
+++ Java_2048_WS/src/java/utils/Arquivo.java
@@ -1,31 +1,33 @@
-
 package utils;
+
 import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import modelos.GameCommands;
 
 public class Arquivo {
-    public static String Read(String Caminho){
+
+    public static String Read(String Caminho) {
         String conteudo = "";
         try {
-            
+
             FileReader arq = new FileReader(Caminho);
             BufferedReader lerArq = new BufferedReader(arq);
             String linha = "";
-            
+
             try {
                 linha = lerArq.readLine();
-                
-                while(linha!=null){
+
+                while (linha != null) {
                     conteudo += linha + "\n";
                     linha = lerArq.readLine();
                 }
                 arq.close();
-                
+                Write(Caminho, "{\n"
+                        + "    \"option\": \"Empty\"\n"
+                        + "}");
                 return conteudo;
             } catch (IOException ex) {
                 System.out.println("Erro: Não foi possível ler o arquivo!");
@@ -36,18 +38,17 @@ public class Arquivo {
             return "";
         }
     }
-    
-    public static boolean Write(String Caminho, String content){
+
+    public static boolean Write(String Caminho, String content) {
         try {
             FileWriter arq = new FileWriter(Caminho);
             PrintWriter gravarArq = new PrintWriter(arq);
             gravarArq.println(content);
             gravarArq.close();
             return true;
-        }catch(IOException e){
+        } catch (IOException e) {
             System.out.println(e.getMessage());
             return false;
         }
     }
 }
-
